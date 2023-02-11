2fcf1fd0170596bf2bbd3915aa9176a9d4041c1a
Tonybrandt <gaston.brandt95@gmail.com>
2023-02-11 22:08:13 +0100

    first commit

---
+++ index7.md
@@ -0,0 +1,170 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Iconos-->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <!--Toastify-->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    <!--CSS-->
+    <link rel="stylesheet" href="estilos.css">
+
+    
+    <title>Mis libros</title>
+</head>
+<body class="">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Mis Libros</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon "></span>
+          </button>
+      
+          <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+              <form class="d-flex">
+                <input id="buscador"class="form-control me-2" placeholder="Busqueda por titulo/autor" aria-label="Search">
+                <button id="btnBuscar" class="btn btn-outline-success">Buscar</button>
+              </form> 
+              
+            </ul>
+            <ul>
+              <select id="selectOrden" class="form-select form-select-sm" aria-label=".form-select-sm example">
+                <option selected>Ordenar por:</option>
+                <option value="1">Mayor a menor</option>
+                <option value="2">Menor a mayor</option>
+                <option value="3">Alfabeticamente</option>
+              </select>
+            </ul>
+            <ul style="list-style: none;"className="navbar-nav me-auto">  
+              <li className="nav-item " >
+                  <a id="botonCarrito" data-bs-toggle="modal" data-bs-target="#idModalCarrito" class="btn btn-secondary"><i class="fas fa-shopping-cart fa-1x"></i></a>
+              </li>
+          </ul>
+           
+          </div>
+          <div>
+              
+          </div>
+        </div> 
+      </nav>
+
+    
+    <div class="container m-4">
+        <div class="row">
+          <div class="col-3">
+            <div id="fecha"></div>
+          </div> 
+          <div class="col-3">
+            <button id="toggleMode" class="btn btn-dark">Dark</button>
+          </div>  
+          <div class="col-3">
+              <button id="agregarLibro" data-bs-toggle="modal" data-bs-target="#idModalAgregarLibro" class="btn btn-success">Agregar libro</button>
+          </div>
+        </div>
+  
+    </div>
+
+
+        <!-- <div class="container">
+            <div class="row">
+                <div class="col-3">
+                    <button id="verCatalogo" class="btn btn-success">Ver Catálogo</button>
+                </div>
+                <div class="col-3">
+                    <button id="ocultarCatalogo" class="btn btn-success">Ocultar Catálogo</button>
+                </div>
+
+            </div>
+    </div>       -->
+    
+    
+    <div class="container">
+        <div id="coincidencia"></div>
+        <div id="libros" class="row"></div>
+    </div>    
+    
+    
+
+    <!--Modals-->
+    <!--Modal Agregar Libro-->
+    <div class="modal fade" id="idModalAgregarLibro" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Agregar Libro</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="modal-body">
+              <form id="formAgregarLibro">
+                <div class="mb-3">
+                  <label for="titulo" class="form-label">Titulo</label>
+                  <input type="text" class="form-control" id="tituloInput" aria-describedby="titulo">
+                  
+                </div>
+                <div class="mb-3">
+                  <label for="autor" class="form-label">Autor</label>
+                  <input type="text" class="form-control" id="autorInput" aria-describedby="autor">
+                  
+                </div>
+                <div class="mb-3">
+                  <label for="precio" class="form-label">Precio:</label>
+                  <input type="text" class="form-control" id="precioInput" aria-describedby="precio">
+                  
+                </div>
+              </form>
+            </div>
+            
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="text" id="guardarLibroBtn" class="btn btn-success" data-bs-dismiss="modal">Guardar Libro</button>
+            </div>
+          </div>
+        </div>
+      </div>
+  
+      <!--Modal Carrito-->
+      <div class="modal fade" id="idModalCarrito" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Carrito de Compras</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" id="modal-bodyCarrito">
+              
+            </div>
+            <div class="row ml-ato">
+              <p id="precioTotal"></p>
+            </div>
+            <div class="modal-footer">
+              
+              
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="button" data-bs-dismiss="modal" class="btn btn-success" id="botonFinalizarCompra">Finalizar Compra</button>
+            </div>
+          </div>
+        </div>
+      </div>
+<!--Script de Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+</script>
+<!--Script de Sweet alert-->
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!--Toastify-->
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script>
+<!--Luxon-->
+<script src="https://cdn.jsdelivr.net/npm/luxon@2.3.0/build/global/luxon.min.js"></script>
+<!--Archivos JS propios-->
+<!--importante el orden-->
+<script src="JS/class.js"></script>
+<script src="JS/main.js"></script>
+<script src="JS/darkToggle.js"></script>
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,431 @@
+﻿// Tercer pre-entrega: storage, eventos y DOM
+// //----------------------------------------------------------------------------------------
+// //PROYECTO CON DOM:
+
+let bebidasDiv = document.getElementById("bebidas1")
+let verCatalogoBtn = document.getElementById("verCatalogo")
+let ocultarCatalogoBtn = document.getElementById("ocultarCatalogo")
+// *********
+let sugerirBebidaBtn = document.getElementById("sugerirBebidaBtn")
+// *********
+let inputBuscador = document.querySelector("#buscador")
+// let coincidencia = document.getElementById("coincidencia")
+// let selectOrden = document.getElementById("selectOrden")
+let botonCarrito = document.getElementById("botonCarrito")
+let modalBodyCarrito = document.getElementById("modal-bodyCarrito")
+// let precioTotal = document.getElementById("precioTotal")
+
+let productosEnCarrito
+if(localStorage.getItem("carrito")){
+    productosEnCarrito = JSON.parse(localStorage.getItem("carrito"))
+}else{
+    productosEnCarrito = []
+    localStorage.setItem("carrito", productosEnCarrito)
+}
+
+function verCatalogo(array){
+    
+    bebidasDiv.innerHTML = ""
+
+    for(let drink of array){
+    //código para imprimir el array
+        //creamos un div padre de la card
+        let nuevoDrinkDiv = document.createElement("div")
+        nuevoDrinkDiv.className = "col-12 col-md-6 col-lg-4 my-3"
+        nuevoDrinkDiv.innerHTML = `
+        <div id="${drink.id}" class="card" style="width: 18rem;">
+            <img class="card-img-top img-fluid" style="height: 530px; object-fit: cover;"src="assets/${drink.imagen}" alt="${drink.nombre} de ${drink.nombre}">
+            <div class="card-body">
+                <h4 class="card-title">${drink.nombre}</h4>
+                <p>Bebida: ${drink.nombre}</p>
+                <p class="${drink.precio <= 2300 && "ofertaLibro"}">Precio: ${drink.precio}</p>
+                <button id="agregarBtn${drink.id}" class="btn btn-outline-success">Agregar al carrito</button>
+            </div>
+        </div> 
+        `
+        bebidasDiv.appendChild(nuevoDrinkDiv)
+        let agregarBtn = document.getElementById(`agregarBtn${drink.id}`)
+        agregarBtn.onclick = ()=>{
+            
+            agregarAlCarrito(drink)
+        }
+    }
+}
+
+
+
+
+
+function agregarAlCarrito(trago){
+    
+    console.log(`El producto ${trago.nombre} ha sido agregado al carrito y vale ${trago.precio}`)
+    //sumarlo a productosEnCarrito
+    productosEnCarrito.push(trago)
+    //setearlo en storage
+    localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+    console.log(productosEnCarrito)
+    //evaluar si ya existe o no el producto
+}
+
+// ******************************************
+function cargarBebida(array){
+    let inputAutor = document.getElementById("autorInput")
+    let inputTitulo = document.getElementById("tituloInput")
+    // let inputPrecio = document.getElementById("precioInput")
+    
+    //hacerlo con la function constructora
+    const nuevaBebida = new Bebida(array.length+1, inputAutor.value, inputTitulo.value, "new-drink.jpg")
+    console.log(nuevaBebida)
+ 
+    //pushearlo o sumarlo al array
+    array.push(nuevaBebida)
+    //guardar en storage:
+    localStorage.setItem("bebidas", JSON.stringify(array))
+    verCatalogo(array)
+    let formAgregarLibro = document.getElementById("formAgregarLibro")
+   
+    formAgregarLibro.reset()
+
+    //agregado Toastify:
+    Toastify({
+        text: `La bebida ${nuevaBebida.nombre} ha sido agregado al stock`,
+        duration: 2500,
+        gravity: "top",
+        position: "right",
+        style: {
+            background: "linear-gradient(to right, #00b09b, #96c93d)",
+            color: "black"
+          }
+  }).showToast()
+ }
+// **************************************
+
+
+function buscarInfo(buscado, array){
+        //condición compuesta || &&
+        //coincidencia total, ej:
+        // libro.autor.toLowerCase() == buscado.toLowerCase() || libro.titulo.toLowerCase() == buscado.toLowerCase()
+        // quiero una coincidencia parcial: método includes
+    let busquedaArray = array.filter(
+            (drink) => drink.nombre.toLowerCase().includes(buscado.toLowerCase()) || drink.nombre.toLowerCase().includes(buscado.toLowerCase())
+        )
+    if(busquedaArray.length == 0){
+        coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`
+        verCatalogo(busquedaArray)
+    }else{
+        coincidencia.innerHTML = ""
+        verCatalogo(busquedaArray)
+    }
+}
+//     //reemplazar por operador ternario
+    // busquedaArray.length == 0 ? 
+    // (coincidencia.innerHTML = `<h3>No hay coincidencias con su búsqueda</h3>`, verCatalogo(busquedaArray)) 
+    // : (coincidencia.innerHTML = "", verCatalogo(busquedaArray))
+// }
+
+
+function cargarProductosCarrito(array){
+    console.log("Funciona btn render carrito")
+    modalBodyCarrito.innerHTML = ""
+    array.forEach((productoCarrito)=>{
+        
+        modalBodyCarrito.innerHTML += `
+        <div class="card border-primary mb-3" id ="productoCarrito${productoCarrito.id}" style="max-width: 540px;">
+            <img class="card-img-center" height="500px" style="object-fit: cover;" src="assets/${productoCarrito.imagen}" alt="${productoCarrito.nombre}">
+            <div class="card-body">
+                    <h4 class="card-title">${productoCarrito.nombre}</h4>
+                
+                    <p class="card-text">$${productoCarrito.precio}</p> 
+                    <button class= "btn btn-danger" id="botonEliminar${productoCarrito.id}"><i class="fas fa-trash-alt"></i></button>
+            </div>    
+        </div>
+        `
+        })
+     //segundo forEach agregar function eliminar   
+     array.forEach((productoCarrito)=>{
+        document.getElementById(`botonEliminar${productoCarrito.id}`).addEventListener("click", ()=>{
+            // console.log("btn eliminar funciona")
+            //borrar del DOM
+            let cardProducto = document.getElementById(`productoCarrito${productoCarrito.id}`)
+            cardProducto.remove()
+            //eliminar del array
+            //busco prod a eliminar
+            let productoEliminar = array.find(drink => drink.id == productoCarrito.id)
+            console.log(productoEliminar)
+            //busco el indice
+            let posicion = array.indexOf(productoEliminar)
+            console.log(posicion)
+            //splice (posicion donde trabajar, cant de elementos a eliminar)
+            array.splice(posicion, 1)
+            console.log(array)
+            //eliminar storage (volver a setear)
+            localStorage.setItem("carrito", JSON.stringify(array))
+            //recalcular total
+            compraTotal(array)
+        })
+     })
+    compraTotal(array)
+}
+// **********************************************************************
+
+function agregarAlCarrito(drink){
+    //evaluar si ya existe o no el producto
+    let bebidaAgregada = productosEnCarrito.find((elem)=> elem.id == drink.id)
+    if(bebidaAgregada == undefined){
+        console.log(`El producto ${drink.nombre} ha sido agregado al carrito y vale ${drink.precio}`)
+        //sumarlo a productosEnCarrito
+        productosEnCarrito.push(drink)
+        //setearlo en storage
+        localStorage.setItem("carrito", JSON.stringify(productosEnCarrito))
+        // console.log(productosEnCarrito)
+        //sweetalert para experiencia de usuario
+        Swal.fire({
+            title: 'Ha agregado un producto :D',
+            text: `La bebida ${drink.nombre} ha sido agregado`,
+            icon: "info",
+            confirmButtonText: "Gracias!",
+            confirmButtonColor: "green",
+            //milisegundo por medida
+            timer: 3000,
+            //para img
+            imageUrl: `assets/${drink.imagen}`,
+            imageHeight: 200 
+        })
+
+    }else{
+        //el producto ya se encuentra
+        console.log(`El producto ${drink.nombre} ya se encuentra en el carrito`)
+        //OTRA OPCION: logica que acumule cantidad
+        //que me avise que ya está en el carrito
+        Swal.fire({
+            text: `El libro ${drink.nombre} ya existe en el carrito`,
+            icon: "info",
+            timer: 1500,
+            showConfirmButton: false
+        })
+    }
+
+}
+
+// *********************************************************************
+
+// FUNCION QUE MUESTRA EL TOTAL EN EL CARRITO
+
+function compraTotal(array){
+    // reduce
+    // let acumulador = 0
+    // for(let book of array){
+    //     acumulador = acumulador + book.precio
+    // }
+    
+    //acumulador con reduce
+    let total = array.reduce((acc, productoCarrito)=> acc + productoCarrito.precio ,0)
+    console.log("Acc con reduce " + total)
+    //ternario para mostrar en el html
+    total == 0 ?
+    precioTotal.innerHTML = `No hay productos agregados` :
+    precioTotal.innerHTML = `El total del carrito es <strong>${total}</strong>`
+    return total
+}
+
+
+//functions ordenar:
+function ordenarMenorMayor(array){
+    //copia array original, para no modificar bebidas
+    const menorMayor = [].concat(array)
+    menorMayor.sort((a, b)=> a.precio - b.precio)
+    verCatalogo(menorMayor)
+}
+
+function ordenarMayorMenor(array){
+    //array que recibe y lo copia
+    const mayorMenor = [].concat(array)
+    mayorMenor.sort((a,b)=> b.precio - a.precio)
+    verCatalogo(mayorMenor)
+    
+}
+
+function ordenarAlfabeticamenteTitulo(array){
+        const ordenadoAlfabeticamente = [].concat(array)
+        //ordenar algo que tiene un dato string
+        //forma de la a-z ascendente
+        ordenadoAlfabeticamente.sort((a, b) => {
+            if (a.nombre > b.nombre) {
+              return 1
+            }
+            if (a.nombre < b.nombre) {
+              return -1
+            }
+            // a es igual b
+            return 0
+          })
+          verCatalogo(ordenadoAlfabeticamente)
+}
+
+
+//  //EVENTOS:
+sugerirBebidaBtn.addEventListener("click", ()=>{
+    cargarBebida(bebidas)
+
+})
+verCatalogoBtn.onclick = function() {
+    verCatalogo(bebidas)
+    
+}
+
+ocultarCatalogoBtn.addEventListener("dblclick", ()=>{
+    bebidasDiv.innerHTML =""
+})
+
+//por cada evento, averiguar su funcionamiento, luego pasarle function con instrucciones a realizar
+inputBuscador.addEventListener("input", ()=>{
+    buscarInfo(inputBuscador.value.toLowerCase(), bebidas)
+})
+//select para ordenar
+selectOrden.addEventListener("change", ()=>{
+    // console.log(selectOrden.value)
+    if(selectOrden.value == "1"){
+        ordenarMayorMenor(bebidas)
+    }else if(selectOrden.value =="2"){
+        ordenarMenorMayor(bebidas)
+    }else if(selectOrden.value == "3"){
+        ordenarAlfabeticamenteTitulo(bebidas)
+    }else{
+        verCatalogo(bebidas)
+    }
+})
+
+botonCarrito.addEventListener("click", ()=>{
+    cargarProductosCarrito(productosEnCarrito)
+})
+// //CODIGO:
+// verCatalogo(estanteria)
+
+// //CLASE 7 - Operadores avanzados y Librerías
+
+// //OPERADORES AVANZADOS
+// // let num = 8
+// // if(num == 4){
+// //     console.log("El num es 4")
+// // }else{
+// //     console.log("El Num NO es 4")
+// // }
+// // //operador ternario:
+// // //condicion ? lo que se ejecuta si es true : lo que se ejecuta si es False
+// // num == 4 ? console.log("El num es 4. Con ternario") : console.log("El Num NO es 4. Con ternario") 
+
+// // //operador nullish
+// // let busquedaNullish = estanteria.find((book) => book.titulo == "Paula" ) ?? "No tenemos ese libro en stock"
+// // console.log(busquedaNullish)
+
+// // //Acceso condicional:
+// // console.log(libro3?.autor || "El/la autor/a no existe")
+// // let enciclopedia3 
+// // console.log(enciclopedia3?.autor || "El/la autor/a no existe")
+// // console.log("---------------------------")
+// // //DESESTRUCTURACION:
+// // //objects
+// // //nombre exacto de la propiedades
+// // let {autor, titulo, precio, imagen, editorial} = libro2
+// // console.log(autor)
+// // console.log(titulo)
+// // console.log(precio)
+// // console.log(imagen)
+// // //si no existe o no pongo el nombre exacto: undefined
+// // console.log(editorial)
+// // titulo = "Crónica de una muerte anunciada"
+// // console.log(titulo)
+// // //para no afectar object original
+// // // libro2.titulo = "Amor en tiempo de cólera"
+// // // console.log(libro2)
+
+// // //desestructuración con ALIAS
+// // let {autor: author, titulo: title, precio: price} = libro3
+// // console.log(author)
+// // console.log(title)
+// // console.log(price)
+
+// // //desestructuración arrays: POR POSICIÓN
+// // console.log("Arrays")
+// // let numeros = [5, 19, 1993, 7, 23, -25]
+// // let [a, segundo, , ,x] = numeros
+// // console.log(a)
+// // console.log(segundo)
+// // console.log(x)
+
+// // //SPREAD
+// // console.log(estanteria)
+// // console.log(...estanteria)
+// // console.log(numeros)
+// // console.log(...numeros)
+// // //objeto Math
+// // console.log(Math.min(...numeros))
+
+// // //con objetos
+// // let superLibro4 = {
+// //     ...libro4,
+// //     cantPag: 875,
+// //     editorial: "Sudamericana"
+// // }
+// // console.log(superLibro4)
+
+// //LIBRERIAS
+// //Primera librería sweetAlert
+// // Swal.fire({
+// //     title: 'Error!',
+// //     text: 'Do you want to continue',
+// //     icon: 'error',
+// //     confirmButtonText: 'Cool'
+// //   })
+// // Swal.fire({
+// //     title: "Bienvenidos/as",
+// //     text: "Buen día, hoy es 4 del 2",
+// //     confirmButtonText: "Entendido",
+// //     confirmButtonColor: "Green",
+// //     icon: "success"
+// // })
+
+// //SEgunda librería Toastify
+// // Toastify({
+// //     text: "This is a toast",
+// //     duration: 3000,
+// //     destination: "https://github.com/apvarun/toastify-js",
+// //     newWindow: true,
+// //     close: true,
+// //     gravity: "bottom", // `top` or `bottom`
+// //     position: "center", // `left`, `center` or `right`
+// //     stopOnFocus: true, // Prevents dismissing of toast on hover
+// //     style: {
+// //       background: "linear-gradient(to right, #00b09b, #96c93d)",
+// //       color: "black"
+// //     },
+// //     onClick: function(){} // Callback after click
+// //   }).showToast();
+
+
+// //Luxon: 
+// //tercera librería
+// const DateTime = luxon.DateTime
+// // console.log(DateTime)
+
+// // //crear con fecha con iso
+// // const cumple = DateTime.fromISO("1993-07-19")
+// // console.log(cumple)
+
+// // console.log(cumple.year)
+// // console.log(cumple.day)
+// // console.log(cumple.weekdayLong)
+
+// const fechaHoy = DateTime.now()
+// // console.log(fechaHoy)
+// // console.log(fechaHoy.second)
+// // console.log(fechaHoy.day)
+
+// // //formatos:
+// // console.log(fechaHoy.toLocaleString(DateTime.DATE_SHORT))
+// // console.log(fechaHoy.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY))
+// // console.log(fechaHoy.toLocaleString(DateTime.DATE_FULL))
+
+// let fecha = document.getElementById("fecha")
+// let fechaMostrar = fechaHoy.toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
+// fecha.innerHTML = `${fechaMostrar}`
